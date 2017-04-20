9fa32a82121bd8c1b7292b399204b6bfa7738dd6
Shchepkin <shchepkin.alex@gmail.com>
2017-04-20 17:49:20 +0300

    add logs to dataProvider method

--- src/test/java/tmp/dataProvider.java
+++ src/test/java/tmp/dataProvider.java
@@ -1,7 +1,40 @@
 package tmp;
 
+import org.testng.annotations.*;
+import pages.Base;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
- * Created by installer on 4/20/17.
+ * PRECONDITION
+ * - one hub
  */
 public class dataProvider {
+    private Base base;
+
+    @Parameters({ "deviceName_" })
+    @BeforeClass
+    public void init(String deviceName_){
+        base = new Base(deviceName_);
+
+    }
+
+    @Test(dataProvider = "dataMethod")
+    public void negativeLogin (Map param) {
+        System.out.println("Start test");
+        System.out.println("comment: " + param.get("comment"));
+        System.out.println("login: " + param.get("login"));
+        System.out.println("password: " + param.get("password"));
+    }
+
+    @DataProvider
+    public Object[][] dataMethod() {
+        return base.getDataProviderValues("negativeLoginData.json");
+    }
+
+    @AfterClass
+    public void endSuit() {
+        System.out.println("\n@AfterClass");
+    }
 }
