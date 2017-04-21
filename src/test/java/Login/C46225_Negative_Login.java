50548624cbd8f496d022d9b9916397b7cb90b35b
Shchepkin <shchepkin.alex@gmail.com>
2017-04-21 15:05:02 +0300

    create normal log view

--- src/test/java/Login/C46225_Negative_Login.java
+++ src/test/java/Login/C46225_Negative_Login.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class C46225_Negative_Login {
 
-    private String login, pass, server, expectedText, actual;
+    private String login, pass, server, expectedText, actualText;
     private Base base;
 
     @Parameters({ "deviceName_" })
@@ -23,6 +23,10 @@ public class C46225_Negative_Login {
     public void init(String deviceName_){
         base = new Base(deviceName_);
         base.initPageObjects(base.getDriver());
+        server = base.getCredsWithKey("server");
+
+        base.introPage.goToAuthorization();
+        base.loginPage.chooseServer(server);
     }
 
     @DataProvider
@@ -32,19 +36,32 @@ public class C46225_Negative_Login {
 
     @Test(dataProvider = "dataProviderIterator")
     public void parameters (Map param) {
-        Base.log(1, "test started");
 
         login = param.get("login").toString();
         pass = param.get("pass").toString();
-        server = base.getCredsWithKey("server");
 
-        Base.log(1,"comment: " + param.get("comment"));
-        Base.log(1,"login: \"" + login + "\"");
-        Base.log(1,"password: " + pass + "\"");
+        System.out.println(param.get("comment"));
+        System.out.println("login: \"" + login + "\"");
+        System.out.println("password: \"" + pass + "\"");
 
-        base.loginPage.loginToTheServer(login, pass, server);
+        System.out.println("");
+        base.loginPage.loginToTheServer(login, pass);
+
         Assert.assertTrue(base.wait.visibilityOfSnackBar(90, true), "SnackBar is not shown");
-        Base.log(1, "test finished");
+
+        if (login.isEmpty() || pass.isEmpty()){
+            expectedText = base.getLocalizeTextForKey("please_fill_in_all_of_the_required_fields");
+        }else {
+            expectedText = base.getLocalizeTextForKey("Login_bad_credentials0");
+        }
+
+        actualText = base.popUp.getSnackBarText();
+
+        System.out.println("expected: \"" + expectedText + "\"");
+        System.out.println("actual: \"" + actualText + "\"");
+
+        Assert.assertEquals(actualText, expectedText);
+        System.out.println("\n");
     }
 
     @AfterClass
